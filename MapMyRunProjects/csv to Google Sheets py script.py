9e2d94ccf3fb1307d04e20f44812114b33b43a5e
Cbhami <cbhami02@louisville.edu>
2022-08-12 23:05:49 -0600

    upd

--- MapMyRunProjects/csv to Google Sheets py script.py
+++ MapMyRunProjects/csv to Google Sheets py script.py
@@ -3,11 +3,13 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 df = pd.read_csv(r"C:\Users\coleb\OneDrive\Coraline\Resources\csv data\user94403143_workout_history.csv")
+#df = pd.read_csv(r"/Users/colehamilton/Library/CloudStorage/#OneDrive-Personal/Coraline/Resources/csv data/#user94403143_workout_history.csv")
 df['Date Submitted'] = pd.to_datetime(df['Date Submitted'])
 df['Date Submitted'] = df['Date Submitted'].dt.strftime('%Y-%m-%d')
 df['Workout Date'] = pd.to_datetime(df['Workout Date'])
 df['Workout Date'] = df['Workout Date'].dt.strftime('%Y-%m-%d')
 df.to_csv(r'C:\Users\coleb\OneDrive\Coraline\Resources\csv data\user94403143_workout_history.csv', index=False)
+#df.to_csv(r'/Users/colehamilton/Library/CloudStorage/#OneDrive-Personal/Coraline/Resources/csv data/#user94403143_workout_history.csv', index=False)   
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
