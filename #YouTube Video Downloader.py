c52aa11c03a1d8ee7721f3bfb318fb4bca67a464
cbhami <92786913+Cbhami@users.noreply.github.com>
2022-10-19 11:24:36 -0600

    upd

--- #YouTube Video Downloader.py
+++ #YouTube Video Downloader.py
@@ -6,7 +6,7 @@
 #Description: This script will download a YouTube video
 #%pip install pytube
 from pytube import YouTube
-yt = YouTube("https://www.youtube.com/watch?v=Bt3KLEetP1Y")
+yt = YouTube("https://www.youtube.com/watch?v=CZxgiSGwZ1Q&ab_channel=AuburnUniversityHumanResources")
 mp4_files = yt.streams.filter(file_extension = "mp4")
 mp4_369p_files = mp4_files.get_by_resolution("360p")
 mp4_369p_files.download("/Users/cole/Library/CloudStorage/OneDrive-Personal/Coraline")
