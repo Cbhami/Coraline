aebf69af130b420e773ebc6e825a71686907f2a8
cbhami <92786913+Cbhami@users.noreply.github.com>
2022-12-24 03:09:18 -0700

    upd

--- mmr_clean.sql
+++ mmr_clean.sql
@@ -1,3 +1,4 @@
+-- Active: 1670612804657@@localhost@3306@myschema
 SELECT * FROM myschema.mmr_clean;
 
 #ALTER TABLE myschema.mmr_clean DROP COLUMN MyUnknownColumn;
@@ -5,4 +6,6 @@ SELECT * FROM myschema.mmr_clean;
 ALTER TABLE myschema.mmr_clean ADD COLUMN Testing VARCHAR(255);
 
 SELECT * FROM myschema.mmr_clean;
+
+
 
