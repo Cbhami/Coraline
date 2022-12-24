fc3761489df9d31f90c5cdc916cf5c1ac7a6f51d
cbhami <92786913+Cbhami@users.noreply.github.com>
2022-12-24 03:54:57 -0700

    upd

--- mmr_clean.sql
+++ mmr_clean.sql
@@ -1,11 +1,29 @@
 -- Active: 1670612804657@@localhost@3306@myschema
 SELECT * FROM myschema.mmr_clean;
 
-#ALTER TABLE myschema.mmr_clean DROP COLUMN MyUnknownColumn;
+ALTER TABLE myschema.mmr_clean DROP COLUMN Testing;
 
 ALTER TABLE myschema.mmr_clean ADD COLUMN Testing VARCHAR(255);
 
-SELECT * FROM myschema.mmr_clean;
+SELECT * FROM myschema.mmr_clean ORDER BY Date DESC;
+
+INSERT INTO mmr_clean (Date) VALUES ('2022-12-23'); 
+
+DELETE from mmr_clean WHERE `Date` = '2022-12-23';
+
+ALTER TABLE `myschema`.`mmr_clean` 
+CHANGE COLUMN `Max.Pace` `MaxPace` DOUBLE NULL DEFAULT NULL ;
+
+ALTER TABLE mmr_clean CHANGE COLUMN `Avg.Spd` `AvgSpd` DOUBLE NULL DEFAULT NULL; 
+
+INSERT INTO myschema.mmr_clean (Date, Activity, Duration, Distance, AvgPace, MaxPace, AvgSpd, `Cal.Burned`, `Avg.HR`, Steps, mileage, minutes, year) 
+VALUES ('2022-12-23', 'Run', '1750', '3.1', '9.4', '0', '6.3', '410', '146', '4861', 'FiveK', '1750', '2022');    
+
+SELECT * FROM myschema.mmr_clean WHERE `Date` = '2022-12-23';
+
+
+
+
 
 
 
